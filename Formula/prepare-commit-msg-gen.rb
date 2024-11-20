f010de0ccd5882539dbaa3a9ec07d2b1ae2d5fc0
Richard Tuin <richardtuin@gmail.com>
2024-11-20 23:18:53 +0100

    fix(prepare-commit-msg-gen): Add other build deps

--- Formula/prepare-commit-msg-gen.rb
+++ Formula/prepare-commit-msg-gen.rb
@@ -9,6 +9,9 @@ class PrepareCommitMsgGen < Formula
 
   depends_on "python@3.11"
   depends_on "rust" => :build
+  depends_on "numpy"
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
 
   resource "aiohappyeyeballs" do
     url "https://files.pythonhosted.org/packages/bc/69/2f6d5a019bd02e920a3417689a89887b39ad1e350b562f9955693d900c40/aiohappyeyeballs-2.4.3.tar.gz"
